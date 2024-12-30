c6462d314db5af3a610766ac519ea8bd83b3f5c7
M3hTi <mehdinasir502@yahoo.com>
2024-12-30 16:19:21 +0330

    feat: add TV shows functionality with dynamic display and scroll-to-top button

---
+++ scripts/tvShows.js
@@ -0,0 +1,119 @@
+const myApi = 'a4feb3cb322b24cafb3f53a88b4aaeba'
+
+const baseUrl = 'https://api.themoviedb.org/3';
+
+const grid = document.querySelector('.movies-grid');
+
+const genreMap = {
+    28: 'Action',
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    99: 'Documentary',
+    18: 'Drama',
+    10751: 'Family',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystery',
+    10749: 'Romance',
+    878: 'Sci-Fi',
+    53: 'Thriller',
+    10752: 'War',
+    37: 'Western',
+    10765: 'Sci-Fi & Fantasy',
+    10759: 'Action & Adventure'
+};
+
+
+fetch(`${baseUrl}/discover/tv?api_key=${myApi}&sort_by=vote_average.desc&vote_count.gte=100`)
+.then(response => {
+    if(!response.ok) throw new Error(`status : ${response.status}`);
+    return response.json();
+})
+.then(data => {
+    console.log(data); 
+    const tvShows = data.results;
+    console.log(tvShows);
+    showTvShows(tvShows);
+})
+.catch(error => {
+    console.log(error);
+});
+
+
+function showTvShows(arr) {
+    arr.forEach(element => {
+        const tvShowCard = document.createElement('div');
+        tvShowCard.className = 'movie-card';
+        grid.appendChild(tvShowCard);
+
+
+        const tvShowPoster = document.createElement('div');
+        tvShowPoster.className = 'movie-poster';
+        tvShowCard.appendChild(tvShowPoster);
+
+        const poster = document.createElement('img');
+        poster.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`;
+        tvShowPoster.appendChild(poster);
+
+        const overlay = document.createElement('div');
+        overlay.className = 'overlay';
+        tvShowPoster.appendChild(overlay);
+
+        const wishListBtn = document.createElement('button');
+        wishListBtn.className = 'wishlist-btn';
+        overlay.appendChild(wishListBtn);
+
+        const icon = document.createElement('i');
+        icon.className = 'fas fa-heart';
+        wishListBtn.appendChild(icon);
+
+        const span = document.createElement('span');
+        span.textContent = 'wishList';
+        wishListBtn.appendChild(span);
+
+        const tvShowsInfo = document.createElement('div');
+        tvShowsInfo.className = 'movie-info';
+        tvShowCard.appendChild(tvShowsInfo);
+
+        const title = document.createElement('h3');
+        title.textContent = element.name;
+        tvShowsInfo.appendChild(title);
+
+        const tvShowsMeta = document.createElement('div');
+        tvShowsMeta.className = 'movie-meta';
+        tvShowsInfo.appendChild(tvShowsMeta);
+
+
+        const rating = document.createElement('span');
+        rating.className = 'rating';
+
+        const star = document.createElement('i');
+        star.className = 'fas fa-star';
+        rating.appendChild(star);
+
+        rating.appendChild(document.createTextNode(element.vote_average.toFixed(1)));
+        tvShowsMeta.appendChild(rating);
+
+        const year = document.createElement('span');
+        year.className = 'year';
+        year.textContent = element.first_air_date.split('-')[0];
+        tvShowsMeta.appendChild(year);
+
+        const genresShows = document.createElement('div')
+        genresShows.className = 'genre-tags';
+        tvShowsInfo.appendChild(genresShows);
+
+        element.genre_ids.forEach(id => {
+            const genre = document.createElement('span');
+            genre.textContent = genreMap[id];
+            genresShows.appendChild(genre);
+        });
+
+
+
+    });
+}
